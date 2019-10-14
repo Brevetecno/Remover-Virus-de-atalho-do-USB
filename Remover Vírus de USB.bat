e4bf91c10bc8f4aed7c28b8648e83b101a987eb4
Herberth Souza <51496746+Brevetecno@users.noreply.github.com>
2019-10-13 21:09:08 -0300

    Removedor de vírus do Atalho em USB
    Este simples Script irá tentar remover o famoso vírus do atalho de seu Pendriver ou outros dispostivos USB.

--- Remover Vírus de USB.bat
+++ Remover Vírus de USB.bat
@@ -15,7 +15,7 @@ cls
 echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
 ECHO Remover virus de atalho do USB - por Brevetecno
 echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
-echo O processo foi iniciado na unidade USB (%usb_letra%)
+echo Mensagem: O processo foi iniciado na unidade USB (%usb_letra%)
 echo Aguarde...
 echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
 
@@ -25,7 +25,7 @@ attrib -h -r -s /s /d %usb_letra%:\*.*
 
 echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
 echo ---------------------------------------------FIM-------------------------------------------------
-ECHO O processo terminou, verifique o seu USB para ter certeza que o virus foi removido
+ECHO Mensagem: O processo terminou, verifique o seu USB para ter certeza que o virus foi removido.
 echo ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ
 ECHO Aperte uma tecla para sair...
 set /p t=
